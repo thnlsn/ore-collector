069ee47ad38fcdad2b6a7ae30bc5f5979f6d69bb
thnlsn <52093425+thnlsn@users.noreply.github.com>
2019-08-12 17:48:49 -0700

    finished, added win/loss, reset game, etc

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,15 +9,20 @@ var goal = 0;
 var total = 0;
 var diamond = 0;
 var emerald = 0;
-var lapis = 0;
 var redstone = 0;
+var lapis = 0;
 var start = true;
+
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓ UPDATES HTML ELEMENTS
+function update(id, value) {
+    document.getElementById(id).innerHTML = value;
+};
 
 
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓ RANDOMIZER FUNCTION (can take any inputs)
 function randNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
 
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓ STARTS GAME BY RUNNING RANDOMIZER FOR EACH VALUE
@@ -25,31 +30,79 @@ function startGame() {
     goal = randNum(19, 120);
     diamond = randNum(1, 12);
     emerald = randNum(1, 12);
-    lapis = randNum(1, 12);
     redstone = randNum(1, 12);
+    lapis = randNum(1, 12);
+    console.log("The goal is " + goal);
+    console.log("The value for the diamond ore is " + diamond);
+    console.log("The value for the emerald ore is " + emerald);
+    console.log("The value for the redstone ore is " + redstone);
+    console.log("The value for the lapis lazuli ore is " + lapis);
 
-    if (start === true) {
-        start = false;
-    } else if (goal === total) {
-        wins++
-    } else {
-        losses--
-    }
+    update("goal-amount", goal);
+    update("total-amount", total);
+    update("win-count", wins);
+    update("loss-count", losses);
 }
+
 
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓ RUNS THIS FUNCTION ON PAGE LOAD
 startGame();
 
 //▓▓▓▓▓▓▓▓▓▓▓▓▓▓ CLICK EVENTS FOR EACH ORE
+$("#dia-ore").click(function() {
+    addToTotal(diamond);
+    console.log(total);
+    update("total-amount", total);
+    winLossCheck();
+});
+
+$("#em-ore").click(function() {
+    addToTotal(emerald);
+    console.log(total);
+    update("total-amount", total);
+    winLossCheck();
+  });
+
+$("#red-ore").click(function() {
+    addToTotal(redstone);
+    console.log(total);
+    update("total-amount", total);
+    winLossCheck();
+});
+
+$("#blu-ore").click(function() {
+    addToTotal(lapis);
+    console.log(total);
+    update("total-amount", total);
+    winLossCheck();
+});
 
 
+//▓▓▓▓▓▓▓▓▓▓▓▓▓▓ FUNCTION TO ADD TO TOTAL
+function addToTotal(amount) {
+    total = amount + total;
+    return total;
+};
 
-//▓▓▓▓▓▓▓▓▓▓▓▓▓▓ 
 
-
-
-//▓▓▓▓▓▓▓▓▓▓▓▓▓▓ 
-
+//▓▓▓▓▓▓▓▓▓▓▓▓▓▓ FUNCTION TO CHECK IF WIN/LOSS
+function winLossCheck() {
+    if (start === true) {
+        start = false;
+    } else if (goal === total) {
+        wins++
+        update("win-count", wins);
+        total = 0;
+        alert("Congratulations, you won!");
+        startGame();
+    } else if (total > goal) {
+        losses++
+        update("loss-count", losses);
+        total = 0;
+        alert("Sorry, you lost!");
+        startGame();
+    }
+}
 
 
 //▓▓▓▓▓▓▓▓▓▓▓▓▓▓ 
